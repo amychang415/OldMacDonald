9a2947266cb9cc9b6c9a6fde41be686a5ce6f097
amychang415 <AMCHANG6@sfusd.edu>
2018-10-15 08:39:55 -0700

    start

--- Animal.java
+++ Animal.java
@@ -1,4 +1,10 @@
-interface Animal 
-{    
-	   //your code here
+interface Animal {    
+  public String getSound();        
+  public String getType(); 
 }  
+
+void main()
+{
+  Farm myFarm = new Farm();
+  Farm.animalSounds();
+}
--- Chick.java
+++ Chick.java
@@ -1,4 +1,28 @@
-class Chick //implements Animal 
+class Chick implements Animal 
 {     
-     //your code here
+     private String myType;     
+     private String mySound;
+     private String mySoundTwo;      
+     public Chick(String type, String sound, String soundTwo)     {         
+         myType = type;         
+         mySound = sound;
+         mySoundTwo = soundTwo;     
+     }     
+     public Chick()     {         
+         myType = "unknown";         
+         mySound = "unknown";
+         mySoundTwo = "unknown";     
+     }      
+     public String getSound()
+     {
+     	if (Math.random() > 0.5)
+     	{
+     		return mySound;
+     	}
+     	else
+     	{
+     		return mySoundTwo;
+     	}
+     }     
+     public String getType(){return myType;} 
 }
